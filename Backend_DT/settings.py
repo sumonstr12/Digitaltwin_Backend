adb03207f37efe1eaef4b6b50910bf2960c4a233
Sumon Roy <162395226+sumonstr12@users.noreply.github.com>
2026-07-12 10:54:55 +0600

    Set DEBUG to False and update ALLOWED_HOSTS

--- Backend_DT/settings.py
+++ Backend_DT/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-so@8ucnvdyw0g(53m8ggzje_)22m3m(z4z-lox3xxos*dd8(bx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["digitaltwin-backend-nkdl.onrender.com]
 
 
 # Application definition
@@ -138,4 +138,5 @@ STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://digital-twin-124.netlify.app"
 ]
